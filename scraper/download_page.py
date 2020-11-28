9daaf5db05c770043e1ee1a5d0509cb58e4fcdc6
Daniel Suveges <suvi.dani@gmail.com>
2020-11-28 12:44:09 +0000

    increasing sleep to 15s

--- scraper/download_page.py
+++ scraper/download_page.py
@@ -1,11 +1,15 @@
 import logging
 import requests
+import random
 
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
 
 import time
 from bs4 import BeautifulSoup, UnicodeDammit
+
+# from scraper_api import ScraperAPIClient
+
 
 # Problematic questions:
 ## https://www.gyakorikerdesek.hu/szamitastechnika__egyeb-kerdesek__10250568-bluetooth-usb-autoradio-hogyan
@@ -29,6 +33,7 @@ def requests_retry_session(
     adapter = HTTPAdapter(max_retries=retry)
     session.mount('http://', adapter)
     session.mount('https://', adapter)
+    session.headers.update({'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) Szopj le haver. AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36'})
     return session
 
 
@@ -41,30 +46,32 @@ def download_page(URL, session = None):
     '''
 
     # Let's wait to avoid being banned (0.1 leads to ban already).
-    time.sleep(0.5)
+    time.sleep(15)
 
     # If no session is provided we generate session:
-    if not session:
-        session = requests_retry_session()
+    session = requests_retry_session()
 
-    response = session.get(URL,timeout=5)
-
-    # URL to download:
+    # client = ScraperAPIClient(api_key)
+    
+    
+    # URL to downloads:
     try:
-        response = requests.get(URL)
+        # response = client.get(url = URL)
+        response = session.get(URL)
     except ConnectionError:
-        print('[Warning] Problem with connection. After 1 sec. retrying.')
-        time.sleep(1)
-        response = requests.get(URL)
+        print('[Warning] request failed.')
 
     # Returned html document:
-    html = response.content.decode('utf-8')
+    html = response.text
 
     # Html encoded into utf8:
     uhtml = UnicodeDammit(html)
 
     # Creating soup:
     soup = BeautifulSoup(uhtml.unicode_markup, features="html.parser")
+
+    if soup.find('title').text == 'Captcha!':
+        print("[Warning] We have triggered the captcha...")
 
     # check if the returned value contain the html.
     return soup
